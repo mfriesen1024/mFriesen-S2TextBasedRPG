47f222faa6b41339afe6043ce0dced83e0719b79
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-01-15 14:28:11 -0400

    Create Entity.cs

---
+++ mFriesen-S2TextBasedRPG/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mFriesen_S2TextBasedRPG
+{
+    internal class Entity
+    {
+        // Stores entity data that isn’t player/foe/neutral specific.
+        public Vector2 position;
+        public List<Item> inventory;
+        int armorInventoryIndex;
+        int weaponInventoryIndex;
+
+        int hp; // health
+        int ap; // absorption
+        int dr; // damage reduction
+
+    }
+}
