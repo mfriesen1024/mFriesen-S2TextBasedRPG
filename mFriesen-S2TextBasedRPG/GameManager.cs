5e85c577fa61a2c695b7456a4af73e107357b9e5
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-01-15 14:17:33 -0400

    Create GameManager.cs

---
+++ mFriesen-S2TextBasedRPG/GameManager.cs
@@ -0,0 +1,13 @@
+﻿namespace mFriesen_S2TextBasedRPG
+{
+    internal class GameManager
+    {
+        Area[] areas; // stores the areas. (to be loaded from a file)
+        Area currentArea; // tracks the current area
+        Encounter[] encounters; // store encounters (load from file)
+        Encounter currentEncounter;
+        string[] storedDialogue; // store dialogue (load from file)
+        string[] currentDialogue; // store the current dialogue passage to read
+
+    }
+}
