2120642165e73a1ffee4cdd1d971c065c4bfddae
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-01-15 14:24:32 -0400

    Create Encounter.cs

---
+++ mFriesen-S2TextBasedRPG/Encounter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mFriesen_S2TextBasedRPG
+{
+    internal class Encounter
+    {
+        // Encounter stores everything for a specific encounter
+        Foe[] foes;
+        Vector2 startDialogue;
+        Vector2 endDialogue;
+    }
+}
