30df5c21bae2b44bcf909e17c2ded6edb536d1d2
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-01-15 14:22:20 -0400

    Create Area.cs

---
+++ mFriesen-S2TextBasedRPG/Area.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mFriesen_S2TextBasedRPG
+{
+    internal class Area
+    {
+        // Class stores everything for a certain room.
+        string name; // use to determine what files to read to grab data.
+        TriggerZone[] warpTriggers; // warp the player to a new area, based on the warp index.
+        int[] warpIndexes; // use to determine where to warp the player.
+        TriggerZone[] encounterTriggers; // launch encounter on trigger.
+        int[] encounterIndexes; // use to determine what encounter to launch.
+        Encounter[] encounters;
+        //Map map; // house map data.
+
+    }
+}
