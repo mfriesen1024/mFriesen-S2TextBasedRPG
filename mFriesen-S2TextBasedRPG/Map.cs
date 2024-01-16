2142f6886da028426351d57cad7f0ed6439b19bf
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-01-16 09:07:34 -0400

    Create Map.cs

---
+++ mFriesen-S2TextBasedRPG/Map.cs
@@ -0,0 +1,19 @@
+﻿namespace mFriesen_S2TextBasedRPG
+{
+    internal class Map
+    {
+        Entity[] entities;
+        Tile[,] tiles;
+        string fName;
+
+        public Map()
+        {
+
+        }
+        Tile[,] GetMap() { return tiles; }
+        void LoadMap() { }
+        void RenderMap() { }
+        void RenderRegion(Vector2 topLeft, Vector2 bottomRight) { }
+
+    }
+}
