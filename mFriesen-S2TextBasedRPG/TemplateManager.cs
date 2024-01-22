147020ff1a805659b892ce3aca2c8fd90fdb73f5
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-01-22 13:46:52 -0400

    File name management in Template loading.

--- mFriesen-S2TextBasedRPG/TemplateManager.cs
+++ mFriesen-S2TextBasedRPG/TemplateManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,27 @@ namespace mFriesen_S2TextBasedRPG
 {
     static class TemplateManager
     {
+        public static string indexFName = "index";
+
         public static string[] items;
         public static string[] foes;
         public static string[] encounters;
+
+        public static void Load(string[] dirs)
+        {
+            LoadEncounters(dirs[2]);
+        }
+
+        static void LoadEncounters(string dirName)
+        {
+            dirName += "\\"; string _indexFName = dirName + indexFName;
+            string[] fNames;
+
+            if(File.Exists(_indexFName))
+            {
+                fNames = File.ReadAllLines(_indexFName);
+            }
+            else { throw new FileNotFoundException(_indexFName + "Was not found."); }
+        }
     }
 }
