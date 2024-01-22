052493800ca544e0fc2d25177bf4dc382338749a
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-01-22 13:30:52 -0400

    Create TemplateManager.cs

---
+++ mFriesen-S2TextBasedRPG/TemplateManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mFriesen_S2TextBasedRPG
+{
+    static class TemplateManager
+    {
+        public static string[] items;
+        public static string[] foes;
+        public static string[] encounters;
+    }
+}
