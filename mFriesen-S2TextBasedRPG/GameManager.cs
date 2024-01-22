0dc094811ebfa8b01c4c443978b91934fd5d2824
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-01-22 16:38:26 -0400

    Allow for area loading.

--- mFriesen-S2TextBasedRPG/GameManager.cs
+++ mFriesen-S2TextBasedRPG/GameManager.cs
@@ -1,13 +1,30 @@
-﻿namespace mFriesen_S2TextBasedRPG
-{
-    internal class GameManager
-    {
-        Area[] areas; // stores the areas. (to be loaded from a file)
-        Area currentArea; // tracks the current area
-        Encounter[] encounters; // store encounters (load from file)
-        Encounter currentEncounter;
-        string[] storedDialogue; // store dialogue (load from file)
-        string[] currentDialogue; // store the current dialogue passage to read
+﻿using System.IO;
 
+namespace mFriesen_S2TextBasedRPG
+{
+    static class GameManager
+    {
+        // Temporarily load encounter 1 automatically for firstplayable
+        static bool autoRunFirstEncounter;
+
+        static string areasFName;
+        static Area[] areas; // stores the areas. (to be loaded from a file)
+        static Area currentArea; // tracks the current area
+        static Encounter currentEncounter;
+        static string[] storedDialogue; // store dialogue (load from file)
+        static string[] currentDialogue; // store the current dialogue passage to read
+
+        static void LoadAreas()
+        {
+            if(File.Exists(areasFName))
+            {
+                string[] aNames = File.ReadAllLines(areasFName);
+                areas = new Area[aNames.Length];
+                for(int i = 0; i < aNames.Length; i++)
+                {
+                    areas[i] = new Area(aNames[i]);
+                }
+            }
+        }
     }
 }
