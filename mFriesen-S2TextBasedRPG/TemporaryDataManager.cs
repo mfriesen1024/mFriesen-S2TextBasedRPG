2ab0a8d4a3c48ec45fef9862011a281e05f9ab9d
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-01-24 12:34:52 -0400

    Create TemporaryDataManager.cs

---
+++ mFriesen-S2TextBasedRPG/TemporaryDataManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mFriesen_S2TextBasedRPG
+{
+    static class TemporaryDataManager
+    {
+        public static List<Foe> foes;
+        public static List<Area> areas;
+
+        public static void GenerateThings()
+        {
+
+        }
+
+        public static void GenerateFoes()
+        {
+            foes.Add( new Foe(1, 5, 0)); // Create a rather weak creature. Once its vitality goes away, its a 1hit.
+            foes.Add(new Foe(5, 0, 2)); // Create a more resilient creture. It can take a hit or 2.
+            foes.Add(new Foe(15, 10, 5)); // Create a formidable dragon.
+        }
+    }
+}
