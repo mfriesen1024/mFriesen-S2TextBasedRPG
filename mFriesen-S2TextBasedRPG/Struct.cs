0d2c8d3fad6f03c29ac89e44f15ef04037556f19
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-01-24 12:58:58 -0400

    This is probably sloppy, but it should get the job done.

--- mFriesen-S2TextBasedRPG/Struct.cs
+++ mFriesen-S2TextBasedRPG/Struct.cs
@@ -7,22 +7,39 @@ namespace mFriesen_S2TextBasedRPG
         none,
         wall
     }
-    struct TriggerZone
+    struct Trigger
     {
         public Vector2 topCorner;
         public Vector2 bottomCorner;
 
+        public Trigger Clone()
+        {
+            Trigger t = new Trigger();
+            t.topCorner = topCorner.Clone();
+            t.bottomCorner = bottomCorner.Clone();
+            return t;
+        }
     }
     struct Vector2
     {
         public int x, y;
 
         public Vector2(int x, int y) { this.x = x; this.y = y; }
+
+        public Vector2 Clone()
+        {
+            return (Vector2)MemberwiseClone();
+        }
     }
     struct Tile
     {
         public char displayChar;
         public ConsoleColor bg, fg; // Foreground and background colours
         public Hazard hazard;
+
+        public Tile Clone()
+        {
+            return (Tile)MemberwiseClone();
+        }
     }
 }
