968aecccf5abdeb9d7120340ec175ee67fdca3bb
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-01-24 15:09:43 -0400

    Add armor/weapons

--- mFriesen-S2TextBasedRPG/Item.cs
+++ mFriesen-S2TextBasedRPG/Item.cs
@@ -1,6 +1,29 @@
 ﻿namespace mFriesen_S2TextBasedRPG
 {
-    public class Item
+    public class Item // These shouldn't need to be cloned, so we won't bother making that method for these.
     {
+
     }
+
+    public class ArmorItem : Item
+    {
+        public int dr;
+
+        public ArmorItem(int dr = 1)
+        {
+            this.dr = dr;
+        }
+    }
+
+    public class Weapon : Item
+    {
+        public int str;
+
+        public Weapon(int str = 1)
+        {
+            this.str = str;
+        }
+    }
+
+    // need something for pickups. either do that here, or in entity.cs.
 }
