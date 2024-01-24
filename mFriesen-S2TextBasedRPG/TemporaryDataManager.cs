98a09e4cc940eb012447b4224e45659ace10298d
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-01-24 13:56:42 -0400

    Fix NullReferenceException

--- mFriesen-S2TextBasedRPG/TemporaryDataManager.cs
+++ mFriesen-S2TextBasedRPG/TemporaryDataManager.cs
@@ -19,6 +19,7 @@ namespace mFriesen_S2TextBasedRPG
 
         static void GenerateFoes()
         {
+            foes = new List<Foe>();
             foes.Add( new Foe(1, 5, 0)); // Create a rather weak goblin. Once its vitality goes away, its a 1hit.
             foes.Add(new Foe(5, 0, 2)); // Create a more resilient skeleton. It can take a hit or 2.
             foes.Add(new Foe(15, 10, 5)); // Create a formidable dragon.
@@ -26,6 +27,7 @@ namespace mFriesen_S2TextBasedRPG
 
         static void GenerateAreas()
         {
+            areas = new List<Area>();
             Area demoArea = new Area("demoArea");
             Foe[] demoEncounter = { (Foe)foes[0].DeepClone(), (Foe)foes[0].DeepClone(), (Foe)foes[0].DeepClone() };
             demoArea.encounter = demoEncounter;
