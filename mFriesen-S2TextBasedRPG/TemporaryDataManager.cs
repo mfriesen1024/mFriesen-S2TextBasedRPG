672cbb8f8d30f8fa6de5fb3e62e42e07958c4bfa
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-01-24 12:35:14 -0400

    Correct typos in lore.

--- mFriesen-S2TextBasedRPG/TemporaryDataManager.cs
+++ mFriesen-S2TextBasedRPG/TemporaryDataManager.cs
@@ -18,8 +18,8 @@ namespace mFriesen_S2TextBasedRPG
 
         public static void GenerateFoes()
         {
-            foes.Add( new Foe(1, 5, 0)); // Create a rather weak creature. Once its vitality goes away, its a 1hit.
-            foes.Add(new Foe(5, 0, 2)); // Create a more resilient creture. It can take a hit or 2.
+            foes.Add( new Foe(1, 5, 0)); // Create a rather weak goblin. Once its vitality goes away, its a 1hit.
+            foes.Add(new Foe(5, 0, 2)); // Create a more resilient skeleton. It can take a hit or 2.
             foes.Add(new Foe(15, 10, 5)); // Create a formidable dragon.
         }
     }
