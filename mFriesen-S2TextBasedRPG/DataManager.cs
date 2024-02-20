d2c81eff7fe670ea90dcdb6d0915ff71439fb762
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-02-20 09:24:24 -0400

    Add foe loading

--- mFriesen-S2TextBasedRPG/DataManager.cs
+++ mFriesen-S2TextBasedRPG/DataManager.cs
@@ -1,4 +1,5 @@
 ﻿using SimpleLogger;
+using System;
 using System.IO;
 
 namespace mFriesen_S2TextBasedRPG
@@ -7,8 +8,12 @@ namespace mFriesen_S2TextBasedRPG
     {
         // File management
         static string indexFileName = "index.txt";
-        static string[] folderNames; // Note that this needs to be filled before calling any load functions.
+        public static string[] folderNames; // Note that this needs to be filled before calling any load functions.
         static string[] indexFileNames;
+        static int foeFolderIndex = 3;
+
+        // Loaded Data
+        static Foe[] foes; 
 
         static void VerifyFolders() // Verifies that the folders requested exist.
         {
@@ -28,8 +33,29 @@ namespace mFriesen_S2TextBasedRPG
                 if (!File.Exists(folderName))
                 {
                     Log.Write($"Missing file {fileName}, creating it.", logType.warning);
-                    File.Create(fileName);
+                    File.Create(fileName).Close();
                 }
+            }
+        }
+
+        static void LoadFoes()
+        {
+            // Read files and init arrays
+            string[] foeNames = File.ReadAllLines(indexFileNames[foeFolderIndex]);
+            int foeCount = foeNames.Length;
+            foes = new Foe[foeCount];
+
+            for (int i = 0;i < foeCount;i++)
+            {
+                string fName = foeNames[i]; // Get file name
+                string[] dataStrings = File.ReadAllLines(fName); // Read file
+                int[] dataInts = new int[dataStrings.Length]; // Create array with same length as string array
+                for (int j = 0; j < dataStrings.Length; j++)
+                {
+                    try { dataInts[j] = int.Parse(dataStrings[j]); } catch (Exception ignored) { } // Parse the string into an int.
+                }
+
+                foes[i] = new Foe(dataInts[0], dataInts[1], dataInts[2], dataInts[3]);
             }
         }
     }
