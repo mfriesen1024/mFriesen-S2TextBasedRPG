dc8c402d744dcd179fdf661b926b3c1fc68ca24f
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-02-22 19:34:00 -0400

    Add item support.

--- mFriesen-S2TextBasedRPG/DataManager.cs
+++ mFriesen-S2TextBasedRPG/DataManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleLogger;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -11,6 +12,7 @@ namespace mFriesen_S2TextBasedRPG
 
         // Data goes here.
         public static List<StatusEffect> statusEffects;
+        public static List<Item> items;
 
         public static void Startup(string[] directories)
         {
@@ -30,19 +32,54 @@ namespace mFriesen_S2TextBasedRPG
 
         void LoadEffects(string extension)
         {
-            string[] fileNames = File.ReadAllLines(dirs[0] + indexAdd);
+            string dir = dirs[0];
+            string[] fileNames = File.ReadAllLines(dir + indexAdd);
             statusEffects = new List<StatusEffect>();
 
             foreach (string file in fileNames)
             {
-                string[] data = File.ReadAllLines(file + "." + extension);
+                try
+                {
+                    string[] data = File.ReadAllLines(dir + "\\" + file + "." + extension);
 
-                statusEffects.Add(new StatusEffect(
-                    type: (effectType)int.Parse(data[0]),
-                    name: data[1],
-                    timer: int.Parse(data[2]),
-                    value: int.Parse(data[3])
-                    ));
+                    statusEffects.Add(new StatusEffect(
+                        type: (effectType)int.Parse(data[0]),
+                        name: data[1],
+                        timer: int.Parse(data[2]),
+                        value: int.Parse(data[3])
+                        ));
+                }
+                catch (Exception e) { Log.Write("Failed to load an effect: " + e.Message, logType.error); Log.Write(e.StackTrace, logType.debug); }
+            }
+            Log.Write($"Loaded {statusEffects.Count} effects.");
+        }
+
+        enum itemType { armor, weapon }
+
+        void LoadItems(string extension)
+        {
+            string dir = dirs[1];
+            string[] fileNames = File.ReadAllLines(dir + indexAdd);
+            items = new List<Item>();
+
+            foreach (string file in fileNames)
+            {
+                try
+                {
+                    string[] data = File.ReadAllLines(dir + "\\" + file + "." + extension);
+
+                    if (data[0] != "")
+                    {
+                        switch ((itemType)int.Parse(data[0]))
+                        {
+                            case itemType.armor: items.Add(new ArmorItem(int.Parse(data[1]))); break;
+                            case itemType.weapon: items.Add(new WeaponItem(int.Parse(data[1]))); break;
+                            default: throw new NotImplementedException("Unsupported item type.");
+                        }
+                    }
+                    else { throw new NotImplementedException("Currently, only armor and weapons are supported."); }
+                }
+                catch (Exception e) { Log.Write("Failed to load an item: " + e.Message, logType.error); Log.Write(e.StackTrace, logType.debug); }
             }
         }
     }
