7b61200251beb4849395e50e234797feb85866bb
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-02-22 09:22:03 -0400

    Fix status oversight

--- mFriesen-S2TextBasedRPG/HUD.cs
+++ mFriesen-S2TextBasedRPG/HUD.cs
@@ -27,9 +27,9 @@ namespace mFriesen_S2TextBasedRPG
         {
             healthStatus result;
             int ehp = hp + ap;
-            if (ehp > maxhp) { result = healthStatus.Healthy; }
-            else if (ehp > (int)(maxhp * 0.5f)) { result = healthStatus.Injured; }
-            else if (ehp > (int)(maxhp * 0.1f)) { result = healthStatus.Bloodied; }
+            if (ehp >= maxhp) { result = healthStatus.Healthy; }
+            else if (ehp >= (int)(maxhp * 0.5f)) { result = healthStatus.Injured; }
+            else if (ehp >= (int)(maxhp * 0.1f)) { result = healthStatus.Bloodied; }
             else if (ehp > 0) { result = healthStatus.Critical; }
             else { result = healthStatus.Dead; }
 
