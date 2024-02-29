a719e225743ae9640e4ebbf33fe08bf8cf6545cd
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-02-28 20:09:13 -0400

    Create levelmanager

---
+++ mFriesen-S2TextBasedRPG/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mFriesen_S2TextBasedRPG
+{
+    internal class LevelManager
+    {
+        internal static actionResult WallCheck(Vector2 target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
