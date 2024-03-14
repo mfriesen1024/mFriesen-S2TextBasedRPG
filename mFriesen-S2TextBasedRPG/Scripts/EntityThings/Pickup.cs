7d2ebf3b9c5976be0111d33903f5baa5282e6901
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-03-14 09:26:37 -0300

    Refactor restoration pickup

--- mFriesen-S2TextBasedRPG/Scripts/EntityThings/Pickup.cs
+++ mFriesen-S2TextBasedRPG/Scripts/EntityThings/Pickup.cs
@@ -2,29 +2,12 @@
 
 namespace mFriesen_S2TextBasedRPG
 {
+    public enum pickupType { item, restoration }
+    public enum restorationType { hp, ap }
     class Pickup : Entity
     {
-        public enum pickupType { item, restoration }
-        public enum restorationType { hp, ap }
-
-        // References and values
-        public restorationType? rType;
-        public pickupType pType;
-
-        public int? rValue;
-
-        
-        // This constructor should be used for restoration pickups.
-        public Pickup(Vector2 position, restorationType rType, int value)
-        {
-            this.position = position;
-            pType = pickupType.restoration;
-            this.rType = rType; rValue = value;
-
-            SetDefaultValues();
-        }
-
-        // This should be used to set the default values of the item, such as a few nulls, and its default tile.
+        // This should be used to set the default values of the item and its default tile. 
+        // We'll also use this as the template off of which RestorationPickup is made.
         virtual protected void SetDefaultValues()
         {
             // set tile
@@ -38,13 +21,40 @@ namespace mFriesen_S2TextBasedRPG
         // A deep clone method, returning a completely clean duplicate of the pickup;
         public override Entity DeepClone()
         {
+            // If I knew how to switchcase this, I would have. This is intended for redundancy, idk if I even need this.
+            if(this is RestorationPickup) { return ((RestorationPickup)this).DeepClone(); }
+            if(this is ItemPickup) { return ((ItemPickup)this).DeepClone(); }
+
             Pickup e = (Pickup)MemberwiseClone();
             e.position = position.Clone();
             e.displayTile = displayTile.Clone();
-            e.rValue = rValue;
-            e.rType = rType;
-            e.pType = pType;
             return e;
+        }
+    }
+
+    class RestorationPickup : Pickup
+    {
+        // References and values
+        public restorationType rType;
+
+        public int rValue;
+
+        public RestorationPickup(Vector2 position, restorationType rType, int value)
+        {
+            this.position = position;
+            this.rType = rType; rValue = value;
+
+            SetDefaultValues();
+        }
+
+        // Do not override SetDefaultValues.
+
+        public override Entity DeepClone()
+        {
+            RestorationPickup r = (RestorationPickup)MemberwiseClone();
+            r.position = position.Clone();
+            r.displayTile = displayTile.Clone();
+            return r;
         }
     }
 
@@ -54,9 +64,6 @@ namespace mFriesen_S2TextBasedRPG
         public ItemPickup(Vector2 position, Item item)
         {
             this.position = position;
-            rType = null;
-            pType = pickupType.item;
-            rValue = null;
             this.item = item;
 
             SetDefaultValues();
