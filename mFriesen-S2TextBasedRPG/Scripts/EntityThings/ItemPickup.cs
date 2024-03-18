90a7d0420e979f7d9045d3bdb1935c4596953f68
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-03-18 08:53:35 -0300

    Refactor items
    Otherwise silver will get mad.

---
+++ mFriesen-S2TextBasedRPG/Scripts/EntityThings/ItemPickup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace mFriesen_S2TextBasedRPG
+{
+    class ItemPickup : Pickup
+    {
+        public Item item { get; private set; }
+        public ItemPickup(Vector2 position, Item item)
+        {
+            this.position = position;
+            this.item = item;
+
+            SetDefaultValues();
+        }
+
+        protected override void SetDefaultValues()
+        {
+            // set tile
+            displayTile = new Tile();
+            displayTile.displayChar = 'i';
+            displayTile.bg = ConsoleColor.White;
+            displayTile.fg = ConsoleColor.Blue;
+            displayTile.hazard = Hazard.none;
+        }
+
+        public override Entity DeepClone()
+        {
+            ItemPickup e = (ItemPickup)MemberwiseClone();
+            e.position = position.Clone();
+            e.displayTile = displayTile.Clone();
+            e.item = item;
+            return e;
+        }
+    }
+}
