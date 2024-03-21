98d80dbab87904c457bdcb051d203d390e03db70
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-03-21 10:13:23 -0300

    update deepclone

--- mFriesen-S2TextBasedRPG/Scripts/EntityThings/Pickup.cs
+++ mFriesen-S2TextBasedRPG/Scripts/EntityThings/Pickup.cs
@@ -22,6 +22,7 @@ namespace mFriesen_S2TextBasedRPG
             // If I knew how to switchcase this, I would have. This is intended for redundancy, idk if I even need this.
             if(this is RestorationPickup) { return ((RestorationPickup)this).DeepClone(); }
             if(this is ItemPickup) { return ((ItemPickup)this).DeepClone(); }
+            if(this is EffectPickup) { return ((EffectPickup)this).DeepClone(); }
 
             Pickup e = (Pickup)MemberwiseClone();
             e.position = position.Clone();
